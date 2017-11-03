2af2f66f638d2cad1264dc14e5c23a93b3ce278f
Michael Pu <michael.pu123@gmail.com>
2017-11-03 13:05:34 -0400

    added object abstract class

---
+++ src/game/Object.java
@@ -0,0 +1,18 @@
+package game;
+
+import javax.swing.text.html.ImageView;
+
+public abstract class Object {
+
+	protected long mMass;
+	protected Velocity mVelocity;
+	protected Coordinate mPosition;
+	protected ImageView mImage;
+
+	public Object(long mass, Velocity velocity, Coordinate position, ImageView image) {
+		mMass = mass;
+		mVelocity = velocity;
+		mPosition = position;
+		mImage = image;
+	}
+}
--- src/game/Spaceship.java
+++ src/game/Spaceship.java
@@ -1,14 +1,10 @@
 package game;
 
-import javafx.scene.image.ImageView;
+import javax.swing.text.html.ImageView;
 
-public class Spaceship {
+public class Spaceship extends Object {
 
-	private Velocity velocity;
-	private Coordinate position;
-	private long mass;
 	private long engineThrust;
-	private ImageView image;
 
 	private static final long DEFAULT_MASS = 549_100; // kilograms
 	private static final long DEFAULT_ENGINE_THRUST = 7_607_000; // Newtons
@@ -16,10 +12,30 @@ public class Spaceship {
 	private static final double GRAVITY_ACCEL = 9.8; // metres per second per second
 	private static final double INITAL_SPEED = 7900; // metres per second
 	private static final int INITAL_DIRECTION = 180; // degrees
+	private static final Velocity INITAL_VELOCITY = new Velocity(INITAL_SPEED, INITAL_DIRECTION);
 	private static final Coordinate INITAL_POSITION = new Coordinate(100, 100);
 
-	public Spaceship() {
-		velocity = new Velocity(INITAL_SPEED, INITAL_DIRECTION);
-		position = INITAL_POSITION;
+	public Spaceship(ImageView image) {
+		super(DEFAULT_MASS, INITAL_VELOCITY, INITAL_POSITION, image);
+	}
+
+	public void engineOn() {
+
+	}
+
+	public void engineOff() {
+
+	}
+
+	public Coordinate getPosition() {
+		return mPosition;
+	}
+
+	public Velocity getVelocity() {
+		return mVelocity;
+	}
+
+	public long getMass() {
+		return mMass;
 	}
 }
