f657bed92ccb2cfc96d7d8ce5058e7645f3c1bfe
Michael Pu <michael.pu123@gmail.com>
2017-11-12 21:44:12 -0500

    test pixelMetre ratio

--- src/game/Spaceship.java
+++ src/game/Spaceship.java
@@ -89,6 +89,8 @@ public class Spaceship extends Sprite {
 			mGroundLevel = mImageView.getScene().getHeight();
 			mPixelToMetreRatio = mGroundLevel / 2112;
 
+			System.out.println(MAX_IMPACT_SPEED * mPixelToMetreRatio);
+
 			Acceleration curAccel = new Acceleration();
 			Acceleration gravAccel = mPlanet.getPlanetaryAcceleration();
 			gravAccel.setRate(gravAccel.getRate() * deltaTime);
@@ -122,7 +124,7 @@ public class Spaceship extends Sprite {
 					mPosition.setY(mGroundLevel - mSpaceshipHeight);
 				}
 			}
-			mPosition.move(mVelocity, mPixelToMetreRatio);
+			mPosition.move(mVelocity, 1);
 
 			if (mPosition.getY() >= mGroundLevel - mSpaceshipHeight) {
 				if (mVelocity.getSpeed() > MAX_IMPACT_SPEED * mPixelToMetreRatio) {
